2496bb4f8b87573537861cb9b35cf40fc44a0cac
Lawrence Luo <luolawrence1@gmail.com>
2023-03-01 21:07:44 -0800

    checkpoint for season data

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,22 @@ import styles from '../styles/Home.module.css';
 export default function Start() {
   const [results, setResults] = React.useState([]);
 
+  const getSeasonData = async (players, year) => {
+    const url = `https://www.balldontlie.io/api/v1/season_averages?season=${year}`;
+    const playerIdsString = players.map(player => `&player_ids[]=${player.id}`).join("");
+    console.log(url + playerIdsString);
+    const response = await fetch(url + playerIdsString);
+    let data = await response.json();
+    console.log(data);
+  }
+
   const getPlayers = async (query="") => {
     const url = 'https://www.balldontlie.io/api/v1/players?search=';
     const response = await fetch(url + query);
     let players = await response.json();
     players = players.data;
+
+    let seasonData = getSeasonData(players, "2018");
 
     players = players.map((player) => {
         return {
