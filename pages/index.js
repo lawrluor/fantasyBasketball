e8208ebeefd0b071e58ac5d5efb2e8d0e8ed6cd9
EugeneBN <eugene.mokri@gmail.com>
2023-03-15 20:27:21 -0700

    testing season data

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,23 @@ import Head from 'next/head';
 
 import styles from '../styles/Home.module.css';
 
+const KEYS = ['pts', 'reb', 'ast', 'stl', 'blk', 'turnover', 'min', 'games_played', 'fgm', 'fga', 'fg_pct', 'fg3m', 'fg3a', 'fg3_pct', 'ftm', 'fta', 'ft_pct','oreb', 'dreb', 'pf'];
+
 export default function Start() {
   const [results, setResults] = React.useState([]);
 
+  // {"data":[
+  //  {"games_played":30,"player_id":32,"season":2018,"min":"16:46","fgm":2.0,"fga":4.73,"fg3m":0.43,"fg3a":1.73,"ftm":0.6,"fta":0.93,"oreb":0.53,"dreb":2.23,"reb":2.77,"ast":0.57,"stl":0.6,"blk":0.47,"turnover":0.47,"pf":0.97,"pts":5.03,"fg_pct":0.423,"fg3_pct":0.25,"ft_pct":0.643},
+  //  {"games_played":34,"player_id":34,"season":2018,"min":"19:19","fgm":2.41,"fga":6.76,"fg3m":0.85,"fg3a":2.88,"ftm":0.47,"fta":0.82,"oreb":0.35,"dreb":1.5,"reb":1.85,"ast":3.5,"stl":0.53,"blk":0.06,"turnover":0.94,"pf":1.65,"pts":6.15,"fg_pct":0.357,"fg3_pct":0.296,"ft_pct":0.571}
+  // ]}
   const getSeasonData = async (players, year) => {
     const url = `https://www.balldontlie.io/api/v1/season_averages?season=${year}`;
     const playerIdsString = players.map(player => `&player_ids[]=${player.id}`).join("");
-    console.log(url + playerIdsString);
+    // alert(url + playerIdsString);
     const response = await fetch(url + playerIdsString);
     let data = await response.json();
-    console.log(data);
+    data = data.data; 
+    return data;
   }
 
   const getPlayers = async (query="") => {
@@ -21,22 +28,49 @@ export default function Start() {
     let players = await response.json();
     players = players.data;
 
-    let seasonData = getSeasonData(players, "2018");
+    let seasonData = await getSeasonData(players, "2022");
 
+    // Match the player to their season data using player_id and id
     players = players.map((player) => {
-        return {
-          id: player.id,
-          first_name: player.first_name,
-          last_name: player.last_name,
-          height_feet: player.height_feet,
-          height_inches: player.height_inches,
-          position: player.position,
-          weight_pounds: player.weight_pounds
-          // team: [Object],
+      let season_data = {}
+      for (let playerSeason of seasonData) {
+        if (player.id === playerSeason.player_id) {
+          season_data = playerSeason;
         }
+      }
+
+      return {
+        id: player.id,
+        first_name: player.first_name,
+        last_name: player.last_name,
+        height_feet: player.height_feet,
+        height_inches: player.height_inches,
+        position: player.position,
+        weight_pounds: player.weight_pounds,
+        season_data: season_data
+        // team: [Object],
+      }
     });
 
     setResults(players);
+  }
+
+  //  {"games_played":34,"player_id":34,"season":2018,
+  // "min":"19:19","fgm":2.41,"fga":6.76,"fg3m":0.85,"fg3a":2.88,"ftm":0.47,
+  // "fta":0.82,"oreb":0.35,"dreb":1.5,"reb":1.85,"ast":3.5,"stl":0.53,"blk":0.06,
+  // "turnover":0.94,"pf":1.65,"pts":6.15,"fg_pct":0.357,"fg3_pct":0.296,"ft_pct":0.571}
+  const generateSeasonTable = (playerSeasonData) => {
+    return (
+      <table>
+        <tr>
+          {KEYS.map(key => <th className={styles.tableCell}>{key}</th>)}  
+        </tr>
+
+        <tr>
+          {KEYS.map(key => <td className={styles.tableCell}>{playerSeasonData[key]}</td>)}
+        </tr>
+      </table>
+    )
   }
 
   React.useEffect(() => {
@@ -71,7 +105,7 @@ export default function Start() {
                 <p>{player.first_name} {player.last_name}</p>
                 <p>{player.position}</p>
                 <p>{player.height_feet}' {player.height_inches}"</p>
-                {/* <p>{JSON.stringify(player.seasonData)}</p> */}
+                {generateSeasonTable(player.season_data)}
               </li>
             ))}
           </ul>
