ab8d9fa51c4825241d6d01e3f15314f8cd2d10bd
EugeneBN <eugene.mokri@gmail.com>
2023-09-06 19:16:16 -0700

    deleted files and fixed spelling

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import { getArticles } from '../nbaApi';
+import { getArticles } from '../nbaAPI';
 
 import styles from '../styles/Home.module.css';
 
